4122641c75366bf14b0deb821971ab6b52cbe57e
Julien Barnier <julien@nozav.org>
2013-02-14 16:36:15 +0100

    Add hdv2003 and rp99 datasets

---
+++ R/datasets.r
@@ -0,0 +1,22 @@
+#' Histoire de vie 2003
+#'
+#' Sample from 2000 people and 20 variables taken from the \emph{Histoire de
+#' Vie} survey, produced in France in 2003 by INSEE.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 2000 rows and 20 variables
+#' @source \url{http://www.insee.fr/fr/themes/detail.asp?ref_id=fd-HDV03}
+#' @name hdv2003
+NULL
+
+#' 1999 French Census - Cities from the Rhône state
+#' 
+#' Sample from the 1999 french census for the cities of the Rhône state.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format A data frame with 301 rows and 21 variables
+#' @source \url{http://www.insee.fr/fr/bases-de-donnees/default.asp?page=recensements.htm}
+#' @name rp99
+NULL
