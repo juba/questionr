376ecf14aab7420e4f9eb2f8f10a0bd85f44a5f3
Julien Barnier <julien@nozav.org>
2013-02-15 15:01:14 +0100

    Fix for S4 format problem with memisc

--- R/lookfor.r
+++ R/lookfor.r
@@ -28,7 +28,7 @@
 #' lookfor(nes1948, "personal attribute")
 #' @source Based on the behaviour of the \code{lookfor} command in Stata. Future versions might include fuzzey search as featured by the \code{query} function of the \code{memisc} package, which also searches value labels and therefore offers a wider search scope.
 #' @seealso \code{\link{query}} in the \link{memisc} package
-#' @import memisc
+#' @importFrom memisc description
 #' @export
 
 lookfor <- function(data, 
