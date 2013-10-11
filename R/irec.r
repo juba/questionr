423fd489389f76498835e9665bbd185cb5be35b8
Julien Barnier <julien@nozav.org>
2013-10-11 11:57:57 +0200

    Add error checking on arguments

--- R/irec.r
+++ R/irec.r
@@ -8,7 +8,14 @@
 ##' @export
 
 irec <- function(df, oldvar) {
-    assign("*questionr_irec_df*", deparse(substitute(df)), envir=.GlobalEnv)
-    assign("*questionr_irec_oldvar*", deparse(substitute(oldvar)), envir=.GlobalEnv)
+    if (!is.character(df)) df <- deparse(substitute(df))
+    if (!is.data.frame(get(df))) stop(sQuote(paste0(df, ' must be a data frame.')))
+    assign(".questionr_irec_df", df, envir=.GlobalEnv)
+    ## If oldvar is not a character string
+    is_char <- FALSE
+    try(if(is.character(oldvar)) is_char <- TRUE, silent=TRUE)
+    if (!is_char) oldvar <- deparse(substitute(oldvar))
+    if (!(oldvar %in% names(get(df)))) stop(sQuote(paste0(oldvar, ' must be a column of ', df, '.')))    
+    assign(".questionr_irec_oldvar", oldvar, envir=.GlobalEnv)
     shiny::runApp(system.file("irec", package="questionr"))
 }
