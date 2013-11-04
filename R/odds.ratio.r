f6e6ebe9145655ddc6bce1b61bb0997b77dc878c
Joseph <joseph@larmarange.net>
2013-11-04 15:54:29 +0200

    On transforme odds.ratio en une méthode S3

--- R/odds.ratio.r
+++ R/odds.ratio.r
@@ -1,59 +1,85 @@
-#' Odds Ratios for logistic regression
+#' Odds Ratio
 #' 
-#' Computes odds ratios (the exponential of model coefficients), 
-#' their confidence intervals and tests if they differ from 1.
+#' S3 method for odds ratio
 #' 
-#' @param reg an object of class \code{"glm"} (with \code{family=binomial}) or \code{"multinom"}
+#' @aliases odds.ratio odds.ratio.glm odds.ratio.multinom
+#' @param x object from whom odds ratio will be computed
+#' @param ... additional parameters
+#' @author Joseph Larmarange <joseph@@larmarange.net>
+#' @export odds.ratio
+
+
+odds.ratio <-
+	function (x, ...) {
+		UseMethod("odds.ratio")
+	}
+
+#' @rdname odds.ratio
+#' @method odds.ratio glm
+#' @S3method odds.ratio glm
+#' @aliases odds.ratio.glm
 #' @param level the confidence level required
 #' @param digits number of decimal to display
 #' @details
-#' Odds ratio could also be obtained with \code{exp(coef(reg))} and confidence
-#' intervals with \code{exp(confint(reg))}.
+#' Odds ratio could also be obtained with \code{exp(coef(x))} and confidence
+#' intervals with \code{exp(confint(x))}.
 #' 
 #' For models calculated with \code{glm}, p-value are the same as
-#' \code{summary(reg)$coefficients[,4]}. For models calculated with
-#' \code{multinom} (nnet), p-value are calculated according to
-#' \url{http://www.ats.ucla.edu/stat/r/dae/mlogit.htm}.
+#' \code{summary(x)$coefficients[,4]}.
+#' @return
+#' For \code{glm} or \code{multinom} objects, returns odds ratios, 
+#' their confidence interval and tests if they differ from 1.
 #' @examples
 #' data(hdv2003)
 #' reg <- glm(cinema ~ sexe + age, data=hdv2003, family=binomial)
 #' odds.ratio(reg)
 #' @seealso 
-#' \code{\link{glm}} in the \link{stats} package and
-#'  \code{\link[nnet]{multinom}} in the \link[nnet]{nnet} package.
-#' @author Joseph Larmarange <joseph@larmarange.net>
-#' @export odds.ratio
+#' \code{\link{glm}} in the \link{stats} package.
+#' @export odds.ratio.glm
 
-odds.ratio <- function(reg, level=0.95, digits=3) {
-	if ("glm" %in% class(reg)) {
-		if(reg$family$family == "binomial"){
-			r <- cbind(exp(coef(reg)),exp(confint(reg, level=level)),summary(reg)$coefficients[,4])
-			r[,1:3] <- round(r[,1:3],digits=digits)
-			colnames(r)[1] <- "OR"
-			colnames(r)[4] <- "p"
-			printCoefmat(r,signif.stars=TRUE,has.Pvalue=TRUE)
-		} else {
-			stop('reg should be a glm with family=binomial or the result of multinom.')
-		}
-	} else if ("multinom" %in% class(reg)) {
-		coef <- summary(reg)$coefficients
-		ci <- confint(reg,level=level)
-		# From http://www.ats.ucla.edu/stat/r/dae/mlogit.htm
-		z <- summary(reg)$coefficients/summary(reg)$standard.errors
-		p <- p <- (1 - pnorm(abs(z), 0, 1)) * 2
-		d <- dim(ci)
-		r <- array(NA,c(d[1]*d[3],d[2]+2))
-		dimnames(r)[[1]]<-rep("",d[1]*d[3])
-		for (i in 1:d[3]) {
-			fl <- (i-1)*d[1] + 1 #first line
-			ll <- i*d[1] #last line
-			r[fl:ll,] <- cbind(coef[i,],ci[,,i],p[i,])
-			rownames(r)[fl:ll] <- paste0(rownames(coef)[i],"/",colnames(coef))
-		}
-		r[,1:3] <- round(r[,1:3],digits=digits)
-		colnames(r) <- c("OR",dimnames(ci)[[2]],"p")
-		printCoefmat(r,signif.stars=TRUE,has.Pvalue=TRUE)
-	}
-	else 
-		stop('reg should be a glm with family=binomial or the result of multinom.')
-}
+odds.ratio.glm <- 
+  function(x, level=0.95, digits=3) {
+	if (!inherits(x, "glm")) stop("x must be of class 'glm'.")
+  	if(x$family$family != "binomial")
+  		stop('x should be a glm with family=binomial.')
+  	r <- cbind(exp(coef(x)),exp(confint(x, level=level)),summary(x)$coefficients[,4])
+  	r[,1:3] <- round(r[,1:3],digits=digits)
+  	colnames(r)[1] <- "OR"
+  	colnames(r)[4] <- "p"
+  	printCoefmat(r,signif.stars=TRUE,has.Pvalue=TRUE)
+  }
+
+
+#' @rdname odds.ratio
+#' @method odds.ratio multinom
+#' @S3method odds.ratio multinom
+#' @aliases odds.ratio.multinom
+#' @details
+#' For models calculated with \code{multinom} (nnet),
+#' p-value are calculated according to
+#' \url{http://www.ats.ucla.edu/stat/r/dae/mlogit.htm}.
+#' @seealso 
+#' \code{\link[nnet]{multinom}} in the \link[nnet]{nnet} package.
+#' @export odds.ratio.glm
+
+odds.ratio.multinom <- 
+  function(x, level=0.95, digits=3) {
+  	if (!inherits(x, "multinom")) stop("x must be of class 'multinom'.")
+  	coef <- summary(x)$coefficients
+  	ci <- confint(x,level=level)
+  	# From http://www.ats.ucla.edu/stat/r/dae/mlogit.htm
+  	z <- summary(x)$coefficients/summary(x)$standard.errors
+  	p <- p <- (1 - pnorm(abs(z), 0, 1)) * 2
+  	d <- dim(ci)
+  	r <- array(NA,c(d[1]*d[3],d[2]+2))
+  	dimnames(r)[[1]]<-rep("",d[1]*d[3])
+  	for (i in 1:d[3]) {
+  		fl <- (i-1)*d[1] + 1 #first line
+  		ll <- i*d[1] #last line
+  		r[fl:ll,] <- cbind(coef[i,],ci[,,i],p[i,])
+  		rownames(r)[fl:ll] <- paste0(rownames(coef)[i],"/",colnames(coef))
+  	}
+  	r[,1:3] <- round(r[,1:3],digits=digits)
+  	colnames(r) <- c("OR",dimnames(ci)[[2]],"p")
+  	printCoefmat(r,signif.stars=TRUE,has.Pvalue=TRUE)
+  }
