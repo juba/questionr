c9b7585150b8e836915aca6dac2f8f894941c1d5
Julien Barnier <julien@nozav.org>
2013-11-12 16:51:31 +0100

    ImportFrom highr in NAMESPACE, and replace global assignments with options() for shiny apps

--- inst/icut/ui.R
+++ inst/icut/ui.R
@@ -2,9 +2,9 @@ library(shiny)
 library(xtable)
 
 ## Global variables
-df_name <- get(".questionr_icut_df", .GlobalEnv)
+df_name <- getOption("questionr_icut_df")
 df <- get(df_name)
-oldvar_name <- get(".questionr_icut_oldvar", .GlobalEnv)
+oldvar_name <- getOption("questionr_icut_oldvar")
 oldvar <- df[,oldvar_name]
 ## Formatted source variable name
 src_var <- ifelse(grepl(" ", oldvar_name),
@@ -12,8 +12,8 @@ src_var <- ifelse(grepl(" ", oldvar_name),
                   sprintf('%s$%s', df_name, oldvar_name))
 
 ## Flag to display the alert on first time launch
-display_alert <- !exists(".questionr_displayed_alert", .GlobalEnv)
-if (display_alert) assign(".questionr_displayed_alert", FALSE, envir=.GlobalEnv)
+display_alert <- is.null(getOption("questionr_displayed_alert"))
+if (display_alert) options(questionr_displayed_alert=TRUE)
 
 summary_table <- function(v) {
     out <- "<table class='table table-bordered table-condensed' id='sumtable'>"
