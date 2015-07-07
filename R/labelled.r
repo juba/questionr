524534b6b29a34e2df9945495d9a364c8573bf18
larmarange <joseph@larmarange.net>
2015-07-07 21:06:48 +0200

    a minimum set of functions to deal with labelled data
    Imported from sjmisc if available, minimum work (haven only) if sjmisc not installed.
    
    Functions are internal. It will be possible to depends on another package if the future if required.

---
+++ R/labelled.r
@@ -0,0 +1,49 @@
+## Internal function
+## wrapping to sjmisc if available
+## doing a minimum overwise (haven compatibility only)
+
+.get_var_labels <- function(x) {
+  if (requireNamespace("sjmisc", quietly = TRUE))
+    return(sjmisc::get_var_labels(x))
+  else
+    return(attr(x, "label"))
+}
+
+.get_val_labels <- function(x) {
+  if (requireNamespace("sjmisc", quietly = TRUE)) {
+    tmp <- sjmisc::get_val_labels(x, include.values = "n")
+    res <- names(tmp)
+    names(res) <- tmp
+    return(res)
+  }
+    
+  else
+    return(attr(x, "labels"))
+}
+
+.as_factor <- function(x, levels = "l") {
+  if (is.factor(x)) return(x)
+  if (!is.atomic(x)) stop("x should be a vector.")
+  
+  v <- unique(x[!is.na(x)])
+  if (is.null(.get_val_labels(x)))
+    l <- data.frame(values = v, labels = v)
+  else if (levels == "v") 
+    l <- data.frame(values = v, labels = v)
+  else 
+    l <- data.frame(values = .get_val_labels(x), labels = names(.get_val_labels(x)))
+  
+  if (levels == "p")
+    l$labels <- paste0("[", l$values, "] ", l$labels)
+  
+  v <- data.frame(values = v)
+  v$values <- as.character(v$values)
+  l$values <- as.character(l$values)
+  l$labels <- as.character(l$labels)
+  v <- merge(l, v, by = "values", all.x = TRUE, all.y = TRUE)
+  v[is.na(v$labels), "labels"] <- v[is.na(v$labels), "values"] 
+  
+  res <- factor(x, levels = v$values, labels = v$labels)
+  attr(res, "label") <- .get_var_labels(x)
+  return(res)
+}
