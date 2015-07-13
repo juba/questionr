4eda36cd56d4d59886d6bf598a30e1db930425a4
Julien Barnier <julien@nozav.org>
2015-07-13 17:08:51 +0200

    Fix false positives in multi.split, we mustn't grep on a potential substring. Fixes #59.

--- R/table.multi.r
+++ R/table.multi.r
@@ -168,7 +168,8 @@ multi.split <- function (var, split.char="/", mnames = NULL) {
   result <- matrix(data = 0, nrow = length(var), ncol = length(lev))
   char.var <- as.character(var)
   for (i in 1:length(lev)) {
-    result[grep(lev[i], char.var, fixed = TRUE), i] <- 1
+    pattern <- sprintf("(^|%s)%s(%s|$)", split.char, lev[i], split.char)
+    result[grep(pattern, char.var), i] <- 1
   }
   result <- data.frame(result)
   colnames(result) <- mnames
