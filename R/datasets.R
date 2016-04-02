df1869300d893ff3d9706bdaf501dddc27033485
larmarange <joseph@larmarange.net>
2016-04-02 16:47:50 +0200

    A fertility dataset
    English version of data(fecondite)

--- R/datasets.R
+++ R/datasets.R
@@ -23,7 +23,7 @@ NULL
 
 #' A fertility survey
 #' 
-#' Some fictive results from a fecondity survey.
+#' Some fictive results from a fecondity survey, with French labels.
 #' 
 #' @docType data
 #' @keywords datasets
@@ -36,6 +36,7 @@ NULL
 #'    }
 #'   
 #'    Data can be linked using the variables \code{id_menage} and \code{id_femme}.
+#' @seealso \link{fertily} for an English version of this dataset.
 #' @name fecondite
 #' @examples 
 #' data(fecondite)
@@ -73,4 +74,60 @@ NULL
 #' @keywords datasets
 #' @format a data frame containing one record for each child of the surveyed women in the \link{fecondite} survey.
 #' @name enfants
+NULL
+
+
+#' A fertility survey
+#' 
+#' Some fictive results from a fecondity survey, with English labels.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format 3 data frames with labelled data (as if data would have been imported from SPSS with \pkg{haven}):
+#'    \itemize{
+#'      \item \code{households} contains some information from the households selected for the survey;
+#'      \item \code{women} contains the questionnaire administered to all 15-49 years old women 
+#'        living in the selected households;
+#'      \item\code{children} contains one record for each child of the surveyed women.
+#'    }
+#'   
+#'    Data can be linked using the variables \code{id_household} and \code{id_woman}.
+#' @seealso \link{fecondite} for an French version of this dataset.
+#' @name fertility
+#' @examples 
+#' data(fertility)
+#' describe(households)
+#' describe(women)
+#' describe(children)
+NULL
+
+#' A fertility survey - "households" table
+#' 
+#' Some fictive results from a fecondity survey.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format a data frame containing some information from the households selected for the \link{fertility} survey.
+#' @name households
+NULL
+
+#' A fertility survey - "women" table
+#' 
+#' Some fictive results from a fecondity survey.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format a data frame containing the questionnaire administered to all 15-49 years old women 
+#'        living in the selected households for the \link{fertility} survey.
+#' @name women
+NULL
+
+#' A fertility survey - "children" table
+#' 
+#' Some fictive results from a fecondity survey.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @format a data frame containing one record for each child of the surveyed women in the \link{fertility} survey.
+#' @name children
 NULL
