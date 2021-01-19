bd589101985da9e06a6278d69b33499d34e16e65
Joseph Larmarange <joseph@larmarange.net>
2021-01-19 16:14:18 +0100

    call labelled in the test

--- tests/testthat/test_describe.R
+++ tests/testthat/test_describe.R
@@ -3,7 +3,8 @@ library(questionr)
 context("describe() method")
 
 test_that("describe() works with a labelled vector of NAs", {
+  skip_if_not_installed("labelled")
   x <- rep(NA_real_, 10)
-  val_labels(x) <- c(no = 0, yes = 1)
+  labelled::val_labels(x) <- c(no = 0, yes = 1)
   expect_error(questionr::describe(x), NA) # no error
 })
